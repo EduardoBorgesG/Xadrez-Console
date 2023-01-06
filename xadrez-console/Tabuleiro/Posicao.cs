927c041843785aaf557d80374295d12194029b42
Borges <borgesguimaraes2001@gmail.com>
2023-01-06 11:13:35 -0300

    Crição tabuleiro

---
+++ xadrez-console/Tabuleiro/Posicao.cs
@@ -0,0 +1,20 @@
+﻿using System.Xml;
+
+namespace Tabuleiro
+{
+    internal class Posicao
+    {
+        public int lina { get; set; }
+        public int coluna { get; set; }
+        public Posicao(int lina, int coluna)
+        {
+            this.lina = lina;
+            this.coluna = coluna;
+        }
+
+        public override string ToString()
+        {
+            return lina + "," + coluna;
+        }
+    }
+}
